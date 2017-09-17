7595ba1513fa559ba01fe9a13a6040b70915461f
ojjsaw <ojjsaw@gmail.com>
2017-09-17 01:40:34 -0700

    RandomForestClassifier, AdaBoostClassifier, KNeighborsClassifier

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -3,6 +3,8 @@
 import matplotlib.pyplot as plt
 from prep_terrain_data import makeTerrainData
 from class_vis import prettyPicture
+from sklearn.metrics import accuracy_score
+from time import time
 
 features_train, labels_train, features_test, labels_test = makeTerrainData()
 
@@ -31,7 +33,47 @@ plt.show()
 ### your code here!  name your classifier object clf if you want the 
 ### visualization code (prettyPicture) to show you the decision boundary
 
+# RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier
+clfrf = RandomForestClassifier(n_estimators=10)
 
+t0 = time()
+clfrf = clfrf.fit(features_train, labels_train)
+print "RandomForestClassifier training time: ", round(time() - t0, 3), "s"
+
+t0 = time()
+pred = clfrf.predict(features_test)
+print "RandomForestClassifier prediction time: ", round(time() - t0, 3), "s"
+
+print "RandomForestClassifier(10): ", accuracy_score(pred, labels_test)
+
+# AdaBoostClassifier
+from sklearn.ensemble import AdaBoostClassifier
+clfad = AdaBoostClassifier(n_estimators=50)
+
+t0 = time()
+clfad = clfad.fit(features_train, labels_train)
+print "AdaBoostClassifier training time: ", round(time() - t0, 3), "s"
+
+t0 = time()
+pred = clfad.predict(features_test)
+print "AdaBoostClassifier prediction time: ", round(time() - t0, 3), "s"
+
+print "AdaBoostClassifier(50): ", accuracy_score(pred, labels_test)
+
+# KNeighborsClassifier
+from sklearn.neighbors import KNeighborsClassifier
+clfk = KNeighborsClassifier(n_neighbors=4)
+
+t0 = time()
+clfk = clfk.fit(features_train, labels_train)
+print "KNeighborsClassifier training time: ", round(time() - t0, 3), "s"
+
+t0 = time()
+pred = clfk.predict(features_test)
+print "KNeighborsClassifier prediction time: ", round(time() - t0, 3), "s"
+
+print "KNeighborsClassifier(4): ", accuracy_score(pred, labels_test)
 
 
 
@@ -39,6 +81,5 @@ plt.show()
 
 
 try:
-    prettyPicture(clf, features_test, labels_test)
 except NameError:
     pass

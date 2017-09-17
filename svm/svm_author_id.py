43d17c62308f756e1d6fa88af20f3ef583629b4b
ojjsaw <ojjsaw@gmail.com>
2017-09-16 20:40:00 -0700

    decision tree and svm

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -24,6 +24,32 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.metrics import accuracy_score
+import numpy as np
+
+from sklearn import svm
+clf = svm.SVC(kernel="rbf", C=10000.0)
+
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time: ", round(time() - t0, 3), "s"
+
+t0 = time()
+pred = clf.predict(features_test)
+print "prediction time: ", round(time() - t0, 3), "s"
+
+print accuracy_score(pred, labels_test)
+
+# Check the prediction of element
+print "10:", pred[10]
+print "26:", pred[26]
+print "50:", pred[50]
+
+# Count the occurences in a list
+print "occurance:", np.bincount(pred)
 
 #########################################################
 
